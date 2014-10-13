14be05174958183a1f73f222ce0187fe0cdf4326
Luis Santos <007lads@gmail.com>
2014-10-13 12:52:13 +0100

    few changes

--- aulaPratica3/ArabianNights.java
+++ aulaPratica3/ArabianNights.java
@@ -1,11 +1,11 @@
 public class ArabianNights{
 
 		public static void main(String[] args){
-		MagicLamp arrabia = new MagicLamp(6);
-		Genie alibaba = arrabia.Rub(4);
+		arabiannights.MagicLamp arrabia = new MagicLamp(6);
+		arabiannights.Genie alibaba = arrabia.Rub(4);
 		alibaba.toString();
 		alibaba.grantWish();
 		alibaba.toString();
 	}
-	
+
 }
